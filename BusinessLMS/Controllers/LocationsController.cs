18c12055290c2e9cb0a824955d4480b3e127dfa9
Luis Gonzalez <luis.gonzalez@rockfishinteractive.com>
2012-09-21 09:12:41 -0500

    Cleanup api

--- BusinessLMS/Controllers/LocationsController.cs
+++ BusinessLMS/Controllers/LocationsController.cs
@@ -16,88 +16,29 @@ namespace BusinessLMS.Controllers
     {
         private BusinessLMSContext db = new BusinessLMSContext();
 
-        // GET api/Locations
+        #region States
+
         public IEnumerable<State> GetStates()
         {
             return (from st in db.States where st.StateCode != String.Empty select st).AsEnumerable();
         }
 
-        // GET api/Locations/5
-        public State GetState(string id)
+        #endregion
+
+        #region ZipCode
+
+        public ZIPCode GetZIPCode(string id)
         {
-            State state = db.States.Find(id);
-            if (state == null)
+            ZIPCode zipcode = db.ZIPCodes.Find(id);
+            if (zipcode == null)
             {
                 throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
             }
 
-            return state;
+            return zipcode;
         }
-
-        // PUT api/Locations/5
-        public HttpResponseMessage PutState(string id, State state)
-        {
-            if (ModelState.IsValid && id == state.StateCode)
-            {
-                db.Entry(state).State = EntityState.Modified;
-
-                try
-                {
-                    db.SaveChanges();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    return Request.CreateResponse(HttpStatusCode.NotFound);
-                }
-
-                return Request.CreateResponse(HttpStatusCode.OK);
-            }
-            else
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest);
-            }
-        }
-
-        // POST api/Locations
-        public HttpResponseMessage PostState(State state)
-        {
-            if (ModelState.IsValid)
-            {
-                db.States.Add(state);
-                db.SaveChanges();
-
-                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, state);
-                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = state.StateCode }));
-                return response;
-            }
-            else
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest);
-            }
-        }
-
-        // DELETE api/Locations/5
-        public HttpResponseMessage DeleteState(string id)
-        {
-            State state = db.States.Find(id);
-            if (state == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.NotFound);
-            }
-
-            db.States.Remove(state);
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                return Request.CreateResponse(HttpStatusCode.NotFound);
-            }
-
-            return Request.CreateResponse(HttpStatusCode.OK, state);
-        }
+        
+        #endregion
 
         protected override void Dispose(bool disposing)
         {
