c573dfd4344fd837e67dcbc5ad68b76e1cbb0ee3
Luis Gonzalez <luis.gonzalez@rockfishinteractive.com>
2013-03-31 13:52:16 -0500

    Remove Syncfusion references and more optimization updates

--- BusinessLMSWeb/App_Start/BundleConfig.cs
+++ BusinessLMSWeb/App_Start/BundleConfig.cs
@@ -7,8 +7,10 @@ namespace BusinessLMSWeb
 		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
+
+			#region Scripts
+
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-				//"~/Scripts/jquery-{version}.js",
 						"~/Scripts/jquery.kwicks*",
 						"~/Scripts/jquery.easing*",
 						"~/Scripts/jquery.simplemodal*",
@@ -16,31 +18,32 @@ namespace BusinessLMSWeb
 						"~/Scripts/jquery.showLoading*",
 						"~/Scripts/DataTables-1.9.4/media/js/jquery*"));
 
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+						"~/Scripts/jquery.unobtrusive*",
+						"~/Scripts/jquery.validate*"));
+
 			bundles.Add(new ScriptBundle("~/bundles/calendar").Include(
 						"~/Scripts/fullcalendar*",
 						"~/Scripts/gcal*"));
+
+			bundles.Add(new ScriptBundle("~/bundles/jqueryupload").IncludeDirectory("~/Scripts/FileUpload", "*.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/dropdown").Include("~/Scripts/jquery.dropdown*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/noty").Include(
 						"~/Scripts/noty/jquery.noty.js",
 						"~/Scripts/noty/layouts/bottom.js",
 						"~/Scripts/noty/themes/default.js"));
 
-			/*
-			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-						"~/Scripts/jquery-ui-{version}.js"
-						));
-			*/
+			bundles.Add(new ScriptBundle("~/bundles/pages/home").Include(
+				"~/Scripts/jquery.prettyPhoto.js",
+				"~/Scripts/jquery.flow.1.2.js",
+				"~/Scripts/jquery.showLoading.min.js",
+				"~/Scripts/Home/Index.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.unobtrusive*",
-						"~/Scripts/jquery.validate*"));
+			#endregion
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryupload").IncludeDirectory("~/Scripts/FileUpload", "*.js"));
-
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
+			#region Styles
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 						"~/Content/Metro.css",
@@ -48,17 +51,15 @@ namespace BusinessLMSWeb
 						"~/Content/fullcalendar.css"
 						));
 
-			bundles.Add(new ScriptBundle("~/Content/main/css").Include(
+			bundles.Add(new StyleBundle("~/Content/main/css").Include(
 						"~/Content/main/main.css"));
 
-			bundles.Add(new ScriptBundle("~/bundles/dropdown").Include(
-				"~/Scripts/jquery.dropdown*"
-				));
-
 			bundles.Add(new StyleBundle("~/Content/themes/custom/css").Include(
-						"~/Content/themes/custom-theme/jquery.ui.css"));
+						"~/Content/themes/custom/jquery.ui.css"));
 
-			BundleTable.EnableOptimizations = false;
+			#endregion
+
+			BundleTable.EnableOptimizations = true;
 		}
 	}
 }
--- BusinessLMSWeb/Controllers/DashboardController.cs
+++ BusinessLMSWeb/Controllers/DashboardController.cs
@@ -1,6 +1,4 @@
-﻿using Syncfusion.Mvc.Diagram;
-using System.Collections.Generic;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace BusinessLMSWeb.Controllers
 {
@@ -9,42 +7,7 @@ namespace BusinessLMSWeb.Controllers
 	{
 		public ActionResult Index()
 		{
-			List<Company> model = new CompanyDataContext().Company;
-			return View(model);
-		}
-	}
-
-	public class Company
-	{
-		public string DeptId { get; set; }
-		public string DeptName { get; set; }
-		public string HeadDept { get; set; }
-		public Shapes Shape { get; set; }
-		public int Width { get; set; }
-		public int Height { get; set; }
-	}
-
-	public class CompanyDataContext
-	{
-		public List<Company> Company
-		{
-			get
-			{
-				List<Company> company = new List<Company>();
-				company.Add(new Company() { DeptId = "Client", DeptName = "Client", HeadDept = "0", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Company", DeptName = "Company", HeadDept = "Client", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Plant", DeptName = "Plant", HeadDept = "Company", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Sales", DeptName = "Sales Organisation", HeadDept = "Company", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Purchase", DeptName = "Purchase Organisation", HeadDept = "Company", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Shipping", DeptName = "Shipping Point", HeadDept = "Plant", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "WholeSale", DeptName = "WholeSale Distribution", HeadDept = "Sales", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Internet", DeptName = "Internet Distribution", HeadDept = "Sales", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Division1", DeptName = "Division", HeadDept = "WholeSale", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Division2", DeptName = "Division", HeadDept = "WholeSale", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Division3", DeptName = "Division", HeadDept = "Internet", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				company.Add(new Company() { DeptId = "Division4", DeptName = "Division", HeadDept = "Internet", Height = 50, Width = 150, Shape = Shapes.RoundedRectangle });
-				return company;
-			}
+			return View();
 		}
 	}
 }
